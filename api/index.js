eb3a51e0f8f1fa133bd8668f251bdb7694b912ef
keddo <adu.kedir.abdurahman@gmail.com>
2024-12-01 11:58:00 +0300

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import isOnline from 'is-online'
 import userRoutes from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import morgan from 'morgan';
 dotenv.config();
 const app = express();
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(morgan('dev'));
 // Routes
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRouter);
+
 const PORT = process.env.PORT || 5000;
 
 let online
