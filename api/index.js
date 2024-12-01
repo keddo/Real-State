03546ebbf76ecc0962c4e246eaab045852e6ba37
keddo <adu.kedir.abdurahman@gmail.com>
2024-12-01 11:29:17 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,13 +1,20 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import isOnline from 'is-online'
+dotenv.config();
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
 
 const PORT = process.env.PORT || 5000;
 
+let online
+const main = async () => {
+    online = await isOnline()
+    const DB_URL = online ? process.env.MONGO_DB_URL_REMOTE : process.env.MONGO_DB_URL_LOCAL;
+    await mongoose.connect(DB_URL,);
+}
+
+main().then(() => console.log(`Connected to MongoDB to ${online ? 'Remote' : 'Local'}`)).catch(error => console.error("Error: ", error))
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}`);
 });
